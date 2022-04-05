18fa9cb0040ba2eb00b8afa8d1a0f78b84fa8755
Prateek513 <prateekpandey513@gmail.com>
2022-04-05 12:36:24 +0530

    version 1.0

---
+++ problem1.js
@@ -0,0 +1,4 @@
+let name="john"
+admin="john"
+alert(admin)
+
---
+++ problem3.js
@@ -0,0 +1,11 @@
+if ("0") {
+    alert( 'Hello' )
+  }
+/*let a = 1, 
+b = 1;
+let c=++a;
+let d=b++;
+console.log(c,d)
+console.log(a,b)
+the value of 
+a=2,b=2,c=2,d=1*/
