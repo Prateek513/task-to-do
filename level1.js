a19176da62421d0c9f5a416e908e3d7d250e51f0
Prateek513 <prateekpandey513@gmail.com>
2022-04-07 19:33:13 +0530

    some more answers

--- level1.js
+++
@@ -1,3 +0,0 @@
-/* 1. let fname="john"
-let admin=fname
-alert(admin)*/
---
+++ ljsevel1.js
@@ -0,0 +1,32 @@
+/* 1. let fname="john"
+let admin=fname
+alert(admin)*/
+/*2. const RED = "FF5733"
+const YELLOW = "FFC300"
+console.log(YELLOW)*/
+/*3. let a="10"
+let b="shiv"
+let res=a+b
+console.log(res,typeof res)*/
+/*4.let a=5
+let b=4
+console.log(a>b)/true/problem1
+let fruit1= "apple"
+let fruit2= "pineapple"
+console.log(fruit1>fruit2)/false/problem2
+let c= "2"
+let d= "12"
+let res=c>d
+console.log(res)/true/problem3
+let a=undefined
+let b=null
+console.log(a==b)/true/problem4
+let a=undefined
+let b=null
+console.log(a===b)/false/problem5
+let a=null
+let b="\n0\n"
+console.log(a==b,a===+b)/false for both problems */
+
+
+
